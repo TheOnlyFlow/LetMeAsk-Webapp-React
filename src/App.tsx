bf5cbbae4182d64dfe5e669ba40c0a12c4af2ee1
TheOnlyFlow <guilherme.vit.correa@gmail.com>
2021-06-23 02:00:10 -0300

    router e context

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { createContext, useState } from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+
+export const TestContext = createContext({} as any)
 
 function App() {
+
+  const [value, setValue] = useState('Teste')
+
   return (
-    <h1>Hello world</h1>
+    <BrowserRouter>
+      <TestContext.Provider value={{value, setValue}}>
+        <Route path='/' exact component={Home} />
+        <Route path='/rooms/new' component={NewRoom} />
+      </TestContext.Provider>
+    </BrowserRouter>
+
   );
 }
 
